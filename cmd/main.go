34bcab1ea9e7a9b3bf22211f45cbd7e788efe405
Teddy Kahwaji <teddykahwaji@Teddys-MacBook-Pro.local>
2024-09-28 22:50:16 -0400

    Revert "update ytdlp library"
    This reverts commit 70ea86ee685ddff887ff01f6a9ab3a425aee6853.

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -15,7 +16,6 @@ import (
 	"tunes/pkg/youtube"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/lrstanley/go-ytdlp"
 	"github.com/zmb3/spotify"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2/clientcredentials"
@@ -53,15 +53,23 @@ func newSpotifyWrapperClient(ctx context.Context, clientID string, clientSecret 
 	return sw.NewSpotifyWrapper(&client)
 }
 
-func newYoutubePlayerClient() *ytdlp.Command {
-	return ytdlp.New().
-		DefaultSearch("ytsearch").
-		ExtractAudio().
-		RecodeVideo("mp3").
-		Output("%(extractor)s - %(title)s.mp3").
-		NoKeepVideo().
-		PrintJSON().
-		Continue()
+func writeNetrcFileContent() error {
+	netrcLogin := os.Getenv("YOUTUBE_USER_NAME")
+	netrcPassword := os.Getenv("YOUTUBE_PASSWORD")
+	netrcContent := fmt.Sprintf("machine youtube login %s password %s", netrcLogin, netrcPassword)
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("Unable to find home directory: %v", err)
+	}
+
+	netrcPath := fmt.Sprintf("%s/.netrc", homeDir)
+
+	if err := os.WriteFile(netrcPath, []byte(netrcContent), 0o0600); err != nil {
+		return fmt.Errorf("writing netrc to file: %w", err)
+	}
+
+	return nil
 }
 
 func main() {
@@ -104,6 +112,9 @@ func main() {
 
 	bot.AddHandler(func(session *discordgo.Session, _ *discordgo.Ready) {
 		ctx := context.Background()
+		if err := writeNetrcFileContent(); err != nil {
+			logger.Fatal("writing netrc failed", zap.Error(err))
+		}
 
 		spotifyWrapper := newSpotifyWrapperClient(ctx, clientID, clientSecret)
 		youtubeSearchWrapper, err := youtube.NewYoutubeSearchWrapper(ctx, creds)
@@ -111,12 +122,9 @@ func main() {
 			logger.Fatal("unable to instantiate youtubeWrapperClient", zap.Error(err))
 		}
 
-		youtubePlayerClient := newYoutubePlayerClient()
-
 		musicCogConfig := &music.CogConfig{
 			HttpClient:           &httpClient,
 			SpotifyWrapper:       spotifyWrapper,
-			YoutubePlayerClient:  youtubePlayerClient,
 			Logger:               logger,
 			YoutubeSearchWrapper: youtubeSearchWrapper,
 		}
