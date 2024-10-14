bc636697914151d7f42cadbf16c95664a620af44
Teddy Kahwaji <teddykahwaji86@gmail.com>
2024-10-14 15:48:25 -0400

    only audit on prod

--- internal/audit/events.go
+++ internal/audit/events.go
@@ -1,6 +1,8 @@
 package audit
 
 import (
+	"os"
+
 	"github.com/TeddyKahwaji/spice-tunes-go/internal/embeds"
 	"github.com/TeddyKahwaji/spice-tunes-go/internal/logger"
 	"github.com/bwmarrin/discordgo"
@@ -12,15 +14,19 @@ const (
 )
 
 func (m *AuditCog) guildDeleteEvent(session *discordgo.Session, guildDeleteEvent *discordgo.GuildDelete) {
-	_, err := session.ChannelMessageSendEmbed(guildAuditLogChannelID, embeds.GuildAuditEmbed(guildDeleteEvent.BeforeDelete, false))
-	if err != nil {
-		m.logger.Warn("unable to send guild audit delete event", zap.Error(err), logger.GuildID(guildDeleteEvent.ID))
+	if env := os.Getenv("ENV"); env == "PROD" {
+		_, err := session.ChannelMessageSendEmbed(guildAuditLogChannelID, embeds.GuildAuditEmbed(guildDeleteEvent.BeforeDelete, false))
+		if err != nil {
+			m.logger.Warn("unable to send guild audit delete event", zap.Error(err), logger.GuildID(guildDeleteEvent.ID))
+		}
 	}
 }
 
 func (m *AuditCog) guildJoinedEvent(session *discordgo.Session, guildJoinedEvent *discordgo.GuildCreate) {
-	_, err := session.ChannelMessageSendEmbed(guildAuditLogChannelID, embeds.GuildAuditEmbed(guildJoinedEvent.Guild, true))
-	if err != nil {
-		m.logger.Warn("unable to send guild audit joined event", zap.Error(err), logger.GuildID(guildJoinedEvent.Guild.ID))
+	if env := os.Getenv("ENV"); env == "PROD" {
+		_, err := session.ChannelMessageSendEmbed(guildAuditLogChannelID, embeds.GuildAuditEmbed(guildJoinedEvent.Guild, true))
+		if err != nil {
+			m.logger.Warn("unable to send guild audit joined event", zap.Error(err), logger.GuildID(guildJoinedEvent.Guild.ID))
+		}
 	}
 }
